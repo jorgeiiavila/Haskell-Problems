bced46769208b104a9c86b91f7bcf87f627ca45f
Jorge Iribe <jorgeiribe03@gmail.com>
2020-10-13 00:20:27 -0600

    Fixed problems numeration

--- 99problems.hs
+++ 99problems.hs
@@ -9,27 +9,28 @@ myButLast [] = error "Empty List"
 myButLast (_:[]) = error "Just one element"
 myButLast list = head (tail (reverse list))
 
+-- Problem 3
 elementAt :: [a] -> Int -> a
 elementAt [] _ = error "Out of bounds"
 elementAt (x:_) 1 = x
 elementAt (_:xs) n | n < 1 = error "Index lower than one"
                    | otherwise = elementAt xs (n - 1)
--- Problem 3
+-- Problem 4
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:xs) = 1 + (myLength xs)
 
--- Problem 4
+-- Problem 5
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = (myReverse xs) ++ [x]
 
--- Problem 5
+-- Problem 6
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome list = let zipped = zip list (reverse list)
                         in all (\(x,y) -> x == y) zipped
 
--- Problem 6
+-- Problem 7
 data NestedList a = Elem a | List [NestedList a]
 
 flatten :: NestedList a -> [a]
